9beebd3381c0f4c9d48e5593dd3afd6480c2f9e0
itsokto <36706257+itsokto@users.noreply.github.com>
2021-03-22 01:33:37 +0300

    Set useFindAndModify to false

--- duck-bot/src/index.ts
+++ duck-bot/src/index.ts
@@ -8,11 +8,17 @@ const initialize = async () => {
   mongoose.connect(process.env.DB_CONNECTION_STRING ?? "", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   });
 
   const bot = createBot(process.env.TELEGRAM_TOKEN ?? "");
 
-  bot.launch({ webhook: { domain: process.env.WEBHOOK_DOMAIN, port: Number(process.env.PORT) } });
+  bot.launch({
+    webhook: {
+      domain: process.env.WEBHOOK_DOMAIN,
+      port: Number(process.env.PORT),
+    },
+  });
 };
 
 initialize();

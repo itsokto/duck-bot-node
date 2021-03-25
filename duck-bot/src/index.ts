2359aa4b219cd6e921f0b0622d6bee9c2ea5fb09
itsokto <36706257+itsokto@users.noreply.github.com>
2021-03-25 03:17:04 +0300

    Integrate applicationinsights

--- duck-bot/src/index.ts
+++ duck-bot/src/index.ts
@@ -1,10 +1,13 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import appInsights = require("applicationinsights");
 import * as mongoose from "mongoose";
 import { createBot } from "./duck.bot";
 
 const initialize = async () => {
+  appInsights.setup().start();
+
   mongoose.connect(process.env.DB_CONNECTION_STRING ?? "", {
     useNewUrlParser: true,
     useUnifiedTopology: true,

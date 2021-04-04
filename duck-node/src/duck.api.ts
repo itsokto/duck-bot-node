cb1571b7329452548e1ac18ebd1c427f487da3a6
itsokto <36706257+itsokto@users.noreply.github.com>
2021-04-05 00:02:19 +0300

    Move urls to constants

--- duck-node/src/duck.api.ts
+++ duck-node/src/duck.api.ts
@@ -1,31 +1,24 @@
 import axios, { AxiosInstance, AxiosResponse } from "axios";
 import { DuckImage, DuckResponse, DuckStrict } from "./models";
+import constants from "./constants";
 
 export class DuckApi {
-  private readonly baseURL = "https://duckduckgo.com/";
-  private readonly regexp = new RegExp("vqd='?(?<vqd>[\\d-]+)'?", "mi");
   private _client: AxiosInstance;
 
   constructor() {
     this._client = axios.create({
-      baseURL: this.baseURL,
-      params: { o: "json", f: ",,,", l: "us-en" },
-    });
-
-    this._client.interceptors.request.use((config) => {
-      if (config.url === config.baseURL) {
-        config.params = { q: config.params["q"] };
-      }
-      return config;
+      baseURL: constants.baseURL,
     });
   }
 
   async getToken(query: string): Promise<String> {
     const page = await this._client
-      .get<string>(this.baseURL, { params: { q: query } })
+      .get<string>("", {
+        params: { q: query },
+      })
       .then((res) => res.data);
 
-    const math = this.regexp.exec(page);
+    const math = constants.vqdRegex.exec(page);
     if (math && math.groups) {
       return math.groups["vqd"];
     }
@@ -39,7 +32,7 @@ export class DuckApi {
   ): Promise<AxiosResponse<DuckResponse<DuckImage>>> {
     const vqd = await this.getToken(query);
     return this._client.get<DuckResponse<DuckImage>>("i.js", {
-      params: { q: query, p: strict, vqd },
+      params: { q: query, p: strict, vqd, o: "json", f: ",,,", l: "us-en" },
     });
   }
 

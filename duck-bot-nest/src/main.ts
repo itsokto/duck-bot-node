d87a801df8e7487f2b67670d578cfdf27163811f
itsokto <36706257+itsokto@users.noreply.github.com>
2021-05-03 07:12:47 +0300

    Configure NEST as standalone app

--- duck-bot-nest/src/main.ts
+++ duck-bot-nest/src/main.ts
@@ -4,7 +4,6 @@ import appInsights = require('applicationinsights');
 
 async function bootstrap() {
   appInsights.setup().setAutoDependencyCorrelation(true, true).setSendLiveMetrics(true).start();
-  const app = await NestFactory.create(AppModule);
-  await app.listenAsync(process.env.PORT || 3000);
+  await NestFactory.createApplicationContext(AppModule);
 }
 bootstrap();

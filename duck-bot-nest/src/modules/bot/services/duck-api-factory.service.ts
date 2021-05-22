fc17df3066de89ee6aa53f7c8ecd09f133c0f32a
itsokto <36706257+itsokto@users.noreply.github.com>
2021-05-22 22:54:46 +0300

    Exclude Mac from user agent generation

--- duck-bot-nest/src/modules/bot/services/duck-api-factory.service.ts
+++ duck-bot-nest/src/modules/bot/services/duck-api-factory.service.ts
@@ -10,7 +10,9 @@ import UserAgent from 'user-agents';
 @Injectable()
 export class DuckApiFactory {
   private readonly _chance = new Chance();
-  private readonly _userAgents = new UserAgent((data) => !data.userAgent.includes('Safari'));
+  private readonly _userAgents = new UserAgent(
+    (data) => !data.userAgent.includes('Safari') || !data.userAgent.includes('Mac'),
+  );
   private readonly _proxies: string[] = [];
   private _proxiesCache: Map<string, HttpsProxyAgent> = new Map<string, HttpsProxyAgent>();
 

9222065bef6f564ed22b67d3293006a8c41c1e5a
itsokto <36706257+itsokto@users.noreply.github.com>
2021-05-30 02:36:55 +0300

    Fix session.entity schema

--- duck-bot-nest/src/modules/storage/entities/session.entity.ts
+++ duck-bot-nest/src/modules/storage/entities/session.entity.ts
@@ -5,17 +5,17 @@ import { DuckStrict } from 'duck-node';
 export class SessionEntity {
   @Index({ unique: true })
   @PrimaryColumn({ type: 'varchar', length: 50 })
-  key: string | undefined;
+  key!: string;
 
   @Column({ type: 'varchar', length: 100, nullable: true })
-  query: string | undefined;
+  query?: string;
 
   @Column({ type: 'varchar', length: 150, nullable: true })
-  vqd: string | undefined;
+  vqd?: string;
 
   @Column({ type: 'varchar', length: 300, nullable: true })
-  next: string | undefined;
+  next?: string;
 
   @Column({ default: DuckStrict.Off })
-  strict: DuckStrict | undefined;
+  strict!: DuckStrict;
 }

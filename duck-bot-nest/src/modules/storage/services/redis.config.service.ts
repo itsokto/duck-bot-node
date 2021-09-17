66c95efa2d558dc872a9c2c72ce8494da98eedf1
itsokto <36706257+itsokto@users.noreply.github.com>
2021-09-17 21:21:39 +0300

    Fix redis connection

--- duck-bot-nest/src/modules/storage/services/redis.config.service.ts
+++ duck-bot-nest/src/modules/storage/services/redis.config.service.ts
@@ -2,15 +2,17 @@ import { CacheModuleOptions, CacheOptionsFactory, Injectable } from '@nestjs/com
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentConfig } from '@common/types/environment.config';
 import * as redisStore from 'cache-manager-ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class RedisConfigService implements CacheOptionsFactory {
   constructor(private _configService: ConfigService<EnvironmentConfig>) {}
 
   createCacheOptions(): CacheModuleOptions {
+    const redis = new Redis(this._configService.get('REDIS_URL'));
     return {
       store: redisStore,
-      url: this._configService.get('REDIS_URL'),
+      redisInstance: redis,
       ttl: 5000,
     };
   }
